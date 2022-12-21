93fe4cb52775ab66ff09bde95fbb5680944e7cd5
Moonshot <dotteda@gmail.com>
2022-12-22 06:58:51 +0900

    Initial commit

---
+++ Source/gitTest/gitTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gitTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITTEST_API AgitTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
